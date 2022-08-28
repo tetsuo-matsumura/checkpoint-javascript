2c8fb3cdf0e6b4bd5298ac4eb9f078509897470c
Tetsuo Matsumura <tet.m@hotmail.com>
2022-08-28 19:25:29 -0300

    fix exercicio 3

--- exercicio3.js
+++ exercicio3.js
@@ -3,31 +3,21 @@
 // even são os números da string que são pares, na ordem em que apareceram
 // o valor da chave odd são os números da string que são ímpares, na ordem em que apareceram
 const separaParOuImpar = (numeroString) => {
-  return 
+  return
 }
 
 
 // Não mexer aqui embaixo
 export default [
   'Exercício 3: Separa Par ou Ímpar',
-  separaParOuImpar('25753') === {
-    even: '2',
-    odd: '5753'
-  },
-  separaParOuImpar('22222') === {
-    even: '22222',
-    odd: ''
-  },
-  separaParOuImpar('1111') === {
-    even: '',
-    odd: '1111'
-  },
-  separaParOuImpar('') === {
-    even: '',
-    odd: ''
-  },
-  separaParOuImpar('6547386825904') === {
-    even: '64868204',
-    odd: '57359'
-  },
+  separaParOuImpar('25753')?.even === '2',
+  separaParOuImpar('25753')?.odd === '5753',
+  separaParOuImpar('22222')?.even === '22222',
+  separaParOuImpar('22222')?.odd === '',
+  separaParOuImpar('1111')?.even === '',
+  separaParOuImpar('1111')?.odd === '1111',
+  separaParOuImpar('')?.even === '',
+  separaParOuImpar('')?.odd === '',
+  separaParOuImpar('6547386825904')?.even === '64868204',
+  separaParOuImpar('6547386825904')?.odd === '57359'
 ]
